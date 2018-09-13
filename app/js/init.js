987eda1ef65538ab256b8c447e9668c77c901acd
MHM\chandra.vinay <vinaychandra58@gmail.com>
2018-09-13 14:28:36 +0900

    Osaka campaign updated

---
+++ app/js/init.js
@@ -0,0 +1,66 @@
+﻿
+// Set the date we're counting down to
+var countDownDate = new Date("dec 5, 2018 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+    // Get todays date and time
+    var now = new Date().getTime();
+    
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+    
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+    // Output the result in an element with id="demo"
+    document.querySelector(".days").innerHTML = days;
+    document.querySelector(".hours").innerHTML = hours;
+    document.querySelector(".minutes").innerHTML = minutes;
+    document.querySelector(".seconds").innerHTML = seconds;
+    
+    // If the count down is over, write some text 
+    if (distance < 0) {
+        clearInterval(x);
+        document.querySelector(".countdown").innerHTML = "EXPIRED";
+    }
+
+    document.query
+}, 1000);
+
+// Wait for the DOM to be ready
+$(function() {
+
+    $("#mainForm").validate({
+        rules: {   
+            name: "required",
+            "checkbox[]": {
+                required: true,
+                minlength: 1
+            },
+            email: {
+                required: true,        
+                email: true
+            }
+        },
+        errorPlacement: function(error, element) {
+        },
+        submitHandler: function(form) {
+            form.submit();
+        }
+    });
+
+    $( ".overlay" ).on( "click", function() {
+        $(".osaka-campaign-popup").removeClass("show");
+        $(this).hide();
+    });
+
+    $(".gift-box-wrap").on( "click", function() {
+        $(".osaka-campaign-popup").addClass("show");
+        $(".overlay").show();
+    });
+});
---
+++ gulpfile.js
@@ -0,0 +1,64 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var runSequence = require('run-sequence');
+var del = require('del');
+
+
+// Start browserSync server
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  })
+})
+
+gulp.task('clean:dist', function() {
+  return del.sync('dist');
+})
+
+
+gulp.task('sass', function() {
+  return gulp.src('app/scss/main.scss') // Gets all files ending with .scss in app/scss
+    .pipe(sass())
+    .pipe(gulp.dest('app/dest/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+gulp.task('script', function() {
+  return gulp.src('app/js/*.js') // Gets all files ending with .scss in app/scss
+   // .pipe(sass())
+    .pipe(gulp.dest('app/dest/js'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+
+gulp.watch('app/scss/**/*.scss', ['sass']); 
+
+
+// Watchers
+gulp.task('watch', function() {
+  gulp.watch('app/scss/**/*.scss', ['sass']);
+  gulp.watch('app/*.html', browserSync.reload); 
+  gulp.watch('app/js/**/*.js', browserSync.reload); 
+})
+
+
+gulp.task('default', function(callback) {
+  runSequence(['sass', 'browserSync'], 'watch',
+    callback
+  )
+})
+
+gulp.task('build', function(callback) {
+  runSequence(
+    'clean:dist',
+    'sass',
+    callback
+  )
+})
