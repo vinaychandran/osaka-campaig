1ed592c37643707310c612cd4bf86dfb8ae42a06
MHM\chandra.vinay <vinaychandra58@gmail.com>
2018-10-31 18:33:30 +0900

    Osaka campaign set time to 0

--- app/js/init.js
+++ app/js/init.js
@@ -1,38 +1,38 @@
 ﻿
 // Set the date we're counting down to
-var countDownDate = new Date("dec 5, 2018 15:37:25").getTime();
+// var countDownDate = new Date("oct 31, 2018 18:00:00").getTime();
 
-if($('.countdown').length > 0){
-    // Update the count down every 1 second
-    var x = setInterval(function() {
+// if($('.countdown').length > 0){
+//     // Update the count down every 1 second
+//     var x = setInterval(function() {
 
-        // Get todays date and time
-        var now = new Date().getTime();
+//         // Get todays date and time
+//         var now = new Date().getTime();
         
-        // Find the distance between now and the count down date
-        var distance = countDownDate - now;
+//         // Find the distance between now and the count down date
+//         var distance = countDownDate - now;
         
-        // Time calculations for days, hours, minutes and seconds
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+//         // Time calculations for days, hours, minutes and seconds
+//         var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+//         var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//         var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+//         var seconds = Math.floor((distance % (1000 * 60)) / 1000);
         
-        // Output the result in an element with id="demo"
-        document.querySelector(".days").innerHTML = days;
-        document.querySelector(".hours").innerHTML = hours;
-        document.querySelector(".minutes").innerHTML = minutes;
-        document.querySelector(".seconds").innerHTML = seconds;
+//         // Output the result in an element with id="demo"
+//         document.querySelector(".days").innerHTML = days;
+//         document.querySelector(".hours").innerHTML = hours;
+//         document.querySelector(".minutes").innerHTML = minutes;
+//         document.querySelector(".seconds").innerHTML = seconds;
         
-        // If the count down is over, write some text 
-        if (distance < 0) {
-            clearInterval(x);
-            document.querySelector(".countdown").innerHTML = "EXPIRED";
-        }
+//         // If the count down is over, write some text 
+//         if (distance < 0) {
+//             clearInterval(x);
+//             // document.querySelector(".countdown").innerHTML = '<div class="countdown-time">0</div>';
+//         }
 
-        document.query
-    }, 1000);
-}
+//         document.query
+//     }, 1000);
+// }
 
 // Wait for the DOM to be ready
 $(function() {
@@ -77,10 +77,10 @@ $(function() {
             $(".terms-condition-content").hide('slow');
         });
 
-        $(".gift-box-wrap").on( "click", function() {
-            $(".osaka-campaign-popup").addClass("show");
-            $(".overlay").show();
-        });
+        // $(".gift-box-wrap").on( "click", function() {
+        //     $(".osaka-campaign-popup").addClass("show");
+        //     $(".overlay").show();
+        // });
 
         $(".update span").on( "click", function() {
             $(".osaka-price-popup").addClass("show");
